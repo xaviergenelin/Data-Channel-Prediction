eaed482d4c6ceba88a308636a89cd5bb2eb5e9fd
dberger2 <dberger2@ncsu.edu>
2021-10-19 20:53:01 -0400

    Testing render code pushes to github

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Data-Channel-Prediction
+Project 2
+================
+Dave Bergeron
+10/19/2021
+
+## Test for project 2
+
+Test
---
+++ project 2 render code.R
@@ -0,0 +1,7 @@
+# render code for project 2
+render(
+  "project2.Rmd",
+  output_format = "github_document" (html_preview = FALSE),
+  output_file = "README.md",
+  encoding = "UTF-8"
+)
